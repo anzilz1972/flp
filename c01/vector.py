972c678f8e3f386f26a064d424b5233a01092228
anzilz1972@163.com <anzilz1972@163.com>
2026-03-18 15:53:45 +0800

    20260318

---
+++ c01/vector.py
@@ -0,0 +1,31 @@
+##########################################
+####author:Anzilz1972
+####date: 2026-3-16
+####Python编程：Fluent Python 练习 
+####第一章：示例 1-2
+##########################################
+import math
+
+class Vector:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+    
+    def __repr__(self):
+        return f'Vector({self.x!r},{self.y!r})'
+    
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
+    
+    def __bool__(self):
+        return bool(abs(self))
+    
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+    
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
+    
+
